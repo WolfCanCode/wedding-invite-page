a74e708dce33c10e206c0fde1b893fff8281f226
Lê Ngọc Trường (Tommy) <wolfcancode@gmail.com>
2023-12-09 16:55:21 +0100

    fix: font size

--- my-weeding-page/src/components/Testimonial/index.tsx
+++ my-weeding-page/src/components/Testimonial/index.tsx
@@ -10,7 +10,7 @@ const font2 = Habibi({ weight: '400', subsets: ['latin'] });
 const Testimonial: React.FC = () => {
 
   return (
-    <section className="flex flex-col text-center py-10">
+    <section className="flex flex-col text-center pt-10">
       <div className='flex justify-center min-h-[50px]'>
         <Lottie
           animationData={animation1Data}
@@ -22,8 +22,8 @@ const Testimonial: React.FC = () => {
       <div className='pt-2 text-center justify-center flex flex-row gap-12'>
         <div className={[font1.className, 'text-lg md:text-2xl'].join(' ')}>An House</div>
       </div>
-      <div className='pt-2 text-center justify-center flex flex-row gap-12'>
-        <div className={[font1.className, 'text-lg md:text-5xl'].join(' ')}>Our story</div>
+      <div className='pt-6 text-center justify-center flex flex-row gap-12'>
+        <div className={[font1.className, 'text-3xl md:text-5xl'].join(' ')}>Our story</div>
       </div>
     </section>
   );
