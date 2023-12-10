21e8a74f40be3c4b28a9cfd56387fba1469a4135
Lê Ngọc Trường (Tommy) <wolfcancode@gmail.com>
2023-12-10 14:05:02 +0100

    fix: build

--- my-weeding-page/src/app/api/getList/route.ts
+++ my-weeding-page/src/app/api/getList/route.ts
@@ -4,6 +4,13 @@ import { NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
+export const dynamic = 'auto';
+export const dynamicParams = true;
+export const revalidate = false;
+export const fetchCache = 'only-no-store';
+export const runtime = 'nodejs';
+export const preferredRegion = 'auto';
+
 export async function GET() {
   const users = await prisma.user.findMany();
   return NextResponse.json({ users }, { status: 200 });
